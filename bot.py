ec277c501d23fd623aed4de056a7fdb597586d72
Atikrant Negi {Gamma} <33253101+Atikrant-Negi@users.noreply.github.com>
2018-04-12 18:10:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import discord
 from discord.ext import commands
 bot = commands.Bot(description="I am red queen ,still not red",command_prefix="r_q")
+@bot.command()
+async def ping():
+    await bot.say('pong!')
 bot.run(os.getnv('TOKEN'))
