b2dc26607a847e4cfcf1dda5b31bf314170e206a
Atikrant Negi {Gamma} <33253101+Atikrant-Negi@users.noreply.github.com>
2018-04-12 17:51:21 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,4 @@
+import discord
+from discord.ext import commands
+bot = commands.Bot(description="I am red queen ,still not red",command_prefix="r_q")
+bot.run("NDMzOTU3OTE0NDU4MzI0OTk0.DbDcJw.WexqhtDgRckeKSjV4see0pJJtx4")
