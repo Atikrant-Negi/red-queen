ec37c74dc94b5999c5965a1389848c435d133a09
Atikrant Negi {Gamma} <33253101+Atikrant-Negi@users.noreply.github.com>
2018-04-13 21:38:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ bot = commands.Bot(description="I am red queen ,still not red",command_prefix="r
 @bot.command()
 async def ping():
     """Ping the bot"""
-    await bot.say("pong!")
-    
+    await bot.say(":ping_pong: Pong! Took {0}ms to scan the system".format(round(bot.latency,1)))
+
+@bot.command()
+async def say(msg):
+    """Say something through the bot"""
+    await bot.say(msg)
+
 bot.run(os.getenv('TOKEN'))
