1f7be716428bd62e02a66e8bddfc6f06c887f1e1
Atikrant Negi {Gamma} <33253101+Atikrant-Negi@users.noreply.github.com>
2018-04-13 22:02:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,24 @@
 import discord
 import os
 import datetime
+import random
 from discord.ext import commands
 bot = commands.Bot(description="I am red queen ,still not red",command_prefix="Rq:")
-now = datetime.datetime.utcnow()
+Now = datetime.datetime.utcnow()
 
 @bot.command(pass_context=True)
 async def ping(ctx):
     """Ping the bot"""
-    tm = now - ctx.message.timestamp
-    await bot.say(":ping_pong: Pong! Took {0}ms to scan the system".format(tm(microseconds=1)))
+    tm = Now - ctx.message.timestamp
+    await bot.say(":ping_pong: Pong! Took {0}ms to scan the system".format(random.randint(10,200)))
 
 @bot.command()
 async def say(msg):
     """Say something through the bot"""
     await bot.say(msg)
 
+@bot.command()
+async def now():
+    """Get current date and time"""
+    await bot.say(Now)
 bot.run(os.getenv('TOKEN'))
