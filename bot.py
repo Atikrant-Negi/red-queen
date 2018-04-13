4eee3e21f96cc0a443aa78f425ebcc629518b4f6
Atikrant Negi {Gamma} <33253101+Atikrant-Negi@users.noreply.github.com>
2018-04-13 21:54:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import discord
 import os
+import datetime
 from discord.ext import commands
-bot = commands.Bot(description="I am red queen ,still not red",command_prefix="r_q:")
+bot = commands.Bot(description="I am red queen ,still not red",command_prefix="Rq:")
+now = datetime.datetime.utcnow()
 
-@bot.command()
-async def ping():
+@bot.command(pass_context=True)
+async def ping(ctx):
     """Ping the bot"""
-    await bot.say(":ping_pong: Pong! Took {0}ms to scan the system".format(round(bot.latency,1)))
+    tm = now - ctx.message.timestamp
+    await bot.say(":ping_pong: Pong! Took {0}ms to scan the system".format(tm(microseconds=1)))
 
 @bot.command()
 async def say(msg):
